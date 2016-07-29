e58f9330657a97735505d20cbd6adbb6ac002058
luismetzger <luismetzger@me.com>
2016-07-29 10:05:46 -0600

    validation homework complete.

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -9,8 +9,15 @@ class OwnersController < ApplicationController
   end
 
   def create
-    owner = Owner.create(owner_params)
-    redirect_to owner_path(owner)
+    @owner = Owner.create(owner_params)
+
+    if @owner.save
+      redirect_to @owner
+    else
+      flash[:error] = @owner.errors.full_messages.to_sentence
+
+      render :new
+    end
   end
 
   def show
--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,5 +1,8 @@
 class Owner < ActiveRecord::Base
-  # TODO: add validations
+  validates :first_name, presence: true, length: {maximum: 255}
+  validates :last_name, presence: true, length: {maximum: 255}
+  validates :email, presence: true, uniqueness: true, length: {maximum: 255}, format: { :with => /@/ }
+
 
   before_save :normalize_phone_number
 
